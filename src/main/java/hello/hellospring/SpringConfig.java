ac635d429b69fb1b8c213c30e52d20415fc3baa2
YeJinHong <teepij@naver.com>
2023-08-31 23:43:23 +0900

    AOP 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,18 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-import javax.swing.*;
 
 @Configuration
 public class SpringConfig {
 
     private final MemberRepository memberRepository;
 
-    public SpringConfig(MemberRepository memberRepository){
+    public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
@@ -23,6 +20,12 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    // @Component 대신 아래 코드로 설정 가능
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository(){
